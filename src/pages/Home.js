188a1072f30877467ba64a8e0e47bac6788165aa
luisf-franca <luisfranca@outlook.com>
2023-11-30 00:08:58 -0300

    Implementação do mapa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,78 @@
-import React from 'react';
-import { View, Text, Image, Button, TouchableOpacity, StyleSheet } from 'react-native';
-import HomeStyles from '../styles/HomeStyles';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
+import userMarker from '../../assets/userMarker.png';
+import styles from '../styles/HomeStyles';
 
 const Home = () => {
-    return (
-      <View style={HomeStyles.container}>
-        <Text style={HomeStyles.title}>Eventos próximos</Text>
-        {/* Aqui você pode adicionar a lista de eventos próximos */}
-        <Text style={HomeStyles.subtitle}>Crie um evento</Text>
-        {/* Aqui você pode adicionar o botão ou formulário para criar um evento */}
-      </View>
-    );
+  const [location, setLocation] = useState(null);
+  const mapViewRef = useRef(null);
+  const { width, height } = Dimensions.get('window');
+
+  const vw = width / 100;
+  const vh = height / 100;
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.error('Permissão de localização negada');
+        return;
+      }
+
+      const currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation.coords);
+    } catch (error) {
+      console.error('Erro ao obter a localização atual:', error);
+    }
   };
 
-  export default Home;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Eventos próximos</Text>
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsPointsOfInterest={false}
+          //provider="google"
+          showsUserLocation={false}
+          providerProps={{
+            apiKey: 'AIzaSyCSfIsK_Uv45-Mxn6OkD31qxrr-9cPHuW8',
+          }}
+        >
+          {/* Adicione um marcador na localização atual */}
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title="Você está aqui"
+          >
+            <View style={{ width: vw * 0.1, height: vh * 0.1 }}>
+              <Image
+                source={userMarker}
+                style={{ resizeMode: 'contain' }}
+              />
+            </View>
+          </Marker>
+        </MapView>
+      )}
+      <Text style={styles.subtitle}>Crie um evento</Text>
+      {/* Aqui você pode adicionar o botão ou formulário para criar um evento */}
+    </View>
+  );
+};
+
+export default Home;
--- src/styles/HomeStyles.js
+++ src/styles/HomeStyles.js
@@ -1,23 +1,35 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+import { FontFamily } from "../../GlobalStyle.js";
 
-const HomeStyles = StyleSheet.create({
+const { width, height } = Dimensions.get('window');
 
-        container: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#F0F8FF', // Use a cor da paleta aqui
-        },
-        title: {
-          fontSize: 24,
-          fontWeight: 'bold',
-          marginBottom: 20,
-        },
-        subtitle: {
-          fontSize: 18,
-          fontWeight: 'bold',
-          marginTop: 20,
-        },
-      });
+const vw = width / 100;
+const vh = height / 100;
 
-export default HomeStyles; 
+const styles = StyleSheet.create({
+  
+
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F0F8FF', // Use a cor da paleta aqui
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: '100%',
+    width: '100%'
+  },
+});
+
+export default styles; 
